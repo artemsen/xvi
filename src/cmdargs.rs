29b81e98dab444513cc1bec8a571a06b26a55402
Artem Senichev <artemsen@gmail.com>
2021-10-28 15:47:07 +0300

    Add command line parser with multifile support
    Signed-off-by: Artem Senichev <artemsen@gmail.com>

---
+++ src/cmdargs.rs
@@ -0,0 +1,167 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Artem Senichev <artemsen@gmail.com>
+
+/// Command line arguments.
+pub struct CmdLineArgs {
+    /// Initial cursor offset.
+    pub offset: Option<u64>,
+    /// Flag to print version info.
+    pub version: bool,
+    /// Flag to print help.
+    pub help: bool,
+    /// List of files to open.
+    pub files: Vec<String>,
+}
+
+impl CmdLineArgs {
+    /// Create new instance from current command line arguments.
+    pub fn new() -> Result<Self, String> {
+        // get command line args and skip self executable file name
+        let args: Vec<String> = std::env::args().skip(1).collect();
+        CmdLineArgs::parse(args)
+    }
+
+    /// Parse command line arguments.
+    ///
+    /// # Arguments
+    ///
+    /// * `args` - command line arguments without self file name
+    fn parse(args: Vec<String>) -> Result<Self, String> {
+        let mut instance = Self {
+            files: Vec::new(),
+            offset: None,
+            version: false,
+            help: false,
+        };
+
+        let mut last_index = args.len();
+        let mut it = args.iter().enumerate();
+        while let Some((index, arg)) = it.next() {
+            if !arg.starts_with('-') {
+                last_index = index;
+                break;
+            }
+            match arg.as_ref() {
+                "-o" | "--offset" => {
+                    if let Some((_, text)) = it.next() {
+                        let (start, radix) = if text.starts_with("0x") {
+                            (2 /* skip 0x */, 16)
+                        } else if text.to_lowercase().chars().any(|c| matches!(c, 'a'..='f')) {
+                            (0, 16)
+                        } else {
+                            (0, 10)
+                        };
+                        if let Ok(offset) = u64::from_str_radix(&text[start..], radix) {
+                            instance.offset = Some(offset);
+                        } else {
+                            return Err(format!("Invalid offset value: {}", text));
+                        }
+                    } else {
+                        return Err("Offset not specified".to_string());
+                    }
+                }
+                "-v" | "--version" => {
+                    instance.version = true;
+                }
+                "-h" | "--help" => {
+                    instance.help = true;
+                }
+                "--" => {
+                    last_index = index + 1;
+                    break;
+                }
+                _ => {
+                    return Err(format!("Invalid argument: {}", arg));
+                }
+            };
+        }
+
+        if last_index < args.len() {
+            instance.files = args.into_iter().skip(last_index).collect();
+        }
+
+        Ok(instance)
+    }
+
+    /// Print usage info.
+    pub fn help() {
+        println!("Usage: xvi [OPTION...] FILE...");
+        println!("  -o, --offset ADDRESS   Set initial cursor offset");
+        println!("  -v, --version          Print version info and exit");
+        println!("  -h, --help             Print this help and exit");
+    }
+}
+
+#[test]
+fn test_cmdargs_simple() {
+    let args = ["--help".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(args.help);
+    assert!(!args.version);
+    assert!(args.offset.is_none());
+    assert!(args.files.is_empty());
+
+    let args = ["-v".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(!args.help);
+    assert!(args.version);
+    assert!(args.offset.is_none());
+    assert!(args.files.is_empty());
+
+    let args = ["--version".to_string(), "-h".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(args.help);
+    assert!(args.version);
+    assert!(args.offset.is_none());
+    assert!(args.files.is_empty());
+}
+
+#[test]
+fn test_cmdargs_offset() {
+    let args = ["--offset".to_string(), "0x12345678".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(!args.help);
+    assert!(!args.version);
+    assert!(args.files.is_empty());
+    assert_eq!(args.offset, Some(0x12345678));
+
+    let args = ["--offset".to_string(), "1234567a".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(!args.help);
+    assert!(!args.version);
+    assert!(args.files.is_empty());
+    assert_eq!(args.offset, Some(0x1234567a));
+
+    let args = ["-o".to_string(), "12345678".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(!args.help);
+    assert!(!args.version);
+    assert!(args.files.is_empty());
+    assert_eq!(args.offset, Some(12345678));
+
+    let args = ["-o".to_string()];
+    assert!(CmdLineArgs::parse(args.to_vec()).is_err());
+    let args = ["-o".to_string(), "test".to_string()];
+    assert!(CmdLineArgs::parse(args.to_vec()).is_err());
+}
+
+#[test]
+fn test_cmdargs_files() {
+    let args = ["file".to_string()];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert!(!args.help);
+    assert!(!args.version);
+    assert_eq!(args.files.len(), 1);
+    assert_eq!(args.files.get(0), Some(&"file".to_string()));
+
+    let args = [
+        "-v".to_string(),
+        "--".to_string(),
+        "--file1".to_string(),
+        "file2".to_string(),
+    ];
+    let args = CmdLineArgs::parse(args.to_vec()).unwrap();
+    assert_eq!(args.files.len(), 2);
+    assert_eq!(args.files.get(0), Some(&"--file1".to_string()));
+    assert_eq!(args.files.get(1), Some(&"file2".to_string()));
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 mod ascii;
 mod changes;
+mod cmdargs;
 mod config;
 mod curses;
 mod cursor;
@@ -15,74 +16,31 @@ mod page;
 mod ui;
 mod view;
 
+use cmdargs::CmdLineArgs;
 use config::Config;
 use curses::Curses;
 use editor::Editor;
-use std::collections::HashMap;
-
-struct Argument {
-    short: char,
-    long: &'static str,
-    param: Option<&'static str>,
-    help: &'static str,
-}
-
-#[rustfmt::skip]
-const ARGS: &[Argument] = &[
-    Argument {
-        short: 'o', long: "offset", param: Some("ADDRESS"),
-        help: "Initial cursor position (jump to offset)",
-    },
-    Argument {
-        short: 'v', long: "version", param: None,
-        help: "Print version info and exit",
-    },
-    Argument {
-        short: 'h', long: "help", param: None,
-        help: "Print this help and exit",
-    },
-];
 
 /// Main entry point.
 fn main() {
-    let args = parse_args();
-    let (file, options) = args.unwrap_or_else(|err| {
+    // handle command line arguments
+    let args = CmdLineArgs::new().unwrap_or_else(|err| {
         eprintln!("{}", err);
         std::process::exit(1);
     });
-    if options.contains_key(&'h') {
-        print_help();
+    if args.help {
+        print_version();
+        CmdLineArgs::help();
         return;
     }
-    if options.contains_key(&'v') {
+    if args.version {
         print_version();
         return;
     }
-
-    // check arguments
-    if file.is_empty() {
-        eprintln!("Input file not specified");
+    if args.files.is_empty() {
+        eprintln!("Input files not specified");
         std::process::exit(1);
     }
-
-    // initial cursor position
-    let offset = if let Some(opt) = options.get(&'o') {
-        let mut text = opt.clone();
-        let radix = if text.starts_with("0x") {
-            text = text[2..].to_string();
-            16
-        } else if text.to_lowercase().chars().any(|c| matches!(c, 'a'..='f')) {
-            16
-        } else {
-            10
-        };
-        Some(u64::from_str_radix(&text, radix).unwrap_or_else(|err| {
-            eprintln!("Invalid offset value: {}, {}", opt, err);
-            std::process::exit(1);
-        }))
-    } else {
-        None
-    };
 
     // install custom panic hook to close curses before printing error info
     let default_panic = std::panic::take_hook();
@@ -92,16 +50,16 @@ fn main() {
     }));
 
     // set window title
-    println!("\x1b]0;XVI: {}\x07", file);
+    println!("\x1b]0;XVI: {}\x07", args.files[0]);
 
     let config = Config::load();
     Curses::initialize(&config.colors);
 
-    let mut editor = match Editor::new(&file, offset, &config) {
+    let mut editor = match Editor::new(&args.files[0], args.offset, &config) {
         Ok(editor) => editor,
         Err(err) => {
             Curses::close();
-            eprintln!("{}: {}", err, &file);
+            eprintln!("{}: {}", err, &args.files[0]);
             std::process::exit(1);
         }
     };
@@ -110,79 +68,10 @@ fn main() {
     Curses::close();
 }
 
-/// Parse command line arguments.
-fn parse_args() -> Result<(String, HashMap<char, String>), String> {
-    let mut options = HashMap::new();
-
-    let args: Vec<String> = std::env::args().collect();
-    let mut it = args.iter().enumerate();
-    it.next(); // skip self file name
-    let mut last: usize = 1;
-    while let Some((argn, argv)) = it.next() {
-        if argv == "--" {
-            last = argn + 1;
-            break;
-        }
-        if !argv.starts_with('-') {
-            last = argn;
-            break;
-        }
-        let mut valid = false;
-        for opt in ARGS {
-            if *argv == format!("-{}", opt.short) || *argv == format!("--{}", opt.long) {
-                if let Some(name) = opt.param {
-                    if let Some((argn, argv)) = it.next() {
-                        options.insert(opt.short, argv.clone());
-                        last = argn + 1;
-                    } else {
-                        return Err(format!("Argument {} must be specified with {}", argv, name));
-                    }
-                } else {
-                    options.insert(opt.short, "".to_string());
-                }
-
-                valid = true;
-                break;
-            }
-        }
-        if !valid {
-            return Err(format!("Invalid argument: {}", argv));
-        }
-    }
-
-    // file to open
-    let file = match args.get(last) {
-        Some(f) => f.clone(),
-        None => String::new(),
-    };
-
-    // remaining (unexpected) argument
-    if let Some(unexpected) = args.get(last + 1) {
-        return Err(format!("Unexpected argument: {}", unexpected));
-    }
-
-    Ok((file, options))
-}
-
 /// Print program version.
 fn print_version() {
     println!(
         "XVI - hexadecimal editor ver.{}.",
         env!("CARGO_PKG_VERSION")
     );
-}
-
-/// Print usage info.
-fn print_help() {
-    print_version();
-    println!("Usage: xvi [OPTION...] FILE");
-    for arg in ARGS {
-        let params = format!(
-            "-{}, --{} {}",
-            arg.short,
-            arg.long,
-            if let Some(p) = arg.param { p } else { "" }
-        );
-        println!("  {:<24} {}", params, arg.help);
-    }
 }
