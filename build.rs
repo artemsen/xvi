ddbbde5b6eae925f2c27ae48a3a87f812746f297
Artem Senichev <artemsen@gmail.com>
2021-11-09 11:33:44 +0300

    Fix version format in build script
    Use SemVer compatible format.
    
    Signed-off-by: Artem Senichev <artemsen@gmail.com>

--- build.rs
+++ build.rs
@@ -10,7 +10,22 @@ fn main() {
         .output()
     {
         if output.status.success() {
-            let ver = std::str::from_utf8(&output.stdout[1..]).unwrap().trim();
+            let mut ver = std::str::from_utf8(&output.stdout)
+                .unwrap()
+                .trim()
+                .to_string();
+            if ver.starts_with('v') {
+                ver.remove(0);
+            }
+            let components: Vec<&str> = ver.split('-').collect();
+            if components.len() > 2 {
+                ver = format!(
+                    "{}.{}-{}",
+                    components[0],
+                    components[1],
+                    components[2..].join("-")
+                );
+            }
             println!("cargo:rustc-env=CARGO_PKG_VERSION={}", ver);
         }
     }
